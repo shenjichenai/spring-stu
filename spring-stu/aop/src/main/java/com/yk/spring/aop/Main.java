3c18d34a566c26db440c274d929de700da950062
shenjichenai <y1016339913@163.com>
2019-02-12 23:06:44 +0800

    适配idea

--- spring-stu/aop/src/main/java/com/yk/spring/aop/Main.java
+++ spring-stu/aop/src/main/java/com/yk/spring/aop/Main.java
@@ -7,7 +7,7 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration({ "applicationContext.xml" })
+@ContextConfiguration({ "classpath:applicationContext.xml" })
 public class Main {
 
 	@Autowired
